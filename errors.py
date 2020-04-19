03d21aef29121dc5759b177b52a2b88846ef2605
Cyanoxygen <cyanoxygen2725@gmail.com>
2020-04-19 19:51:27 +0800

    errors: more errors

--- errors.py
+++ errors.py
@@ -1,7 +1,33 @@
+from texts import *
+
 class TagNotFound(BaseException):
-    def __init__(self,tag=''):
-        self.tag = tag
+	'''
+	Will be raised if the tag was not found in the database.
+	'''
+	def __init__(self):
+		pass
 
-    def __str__(self):
-        return f'Tag {self.tag} not found'
+	def __str__(self):
+		return err_tag_not_exist
 
+
+class TagAlreadyExists(BaseException):
+	'''
+	Will be raised if the tag already exists.
+	'''
+	def __init__(self):
+		pass
+
+	def __str__(self):
+		return err_tag_exists
+
+
+class TagNotOwned(BaseException):
+	'''
+	Will be raised if the user does not match the credit.
+	'''
+	def __init__(self):
+		pass
+
+	def __str__(self):
+		return err_not_your_tag
